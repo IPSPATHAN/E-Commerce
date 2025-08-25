d7adf3339883af07bc30eea53f38aaea7cf34bca
IPSPATHAN <157199621+IPSPATHAN@users.noreply.github.com>
2025-08-26 02:42:22 +0530

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🚀 MERN E-Commerce Project</h1>
+      <p>Welcome to my MERN App deployed on Netlify + Render.</p>
+    </div>
+  );
+}
+
+export default App;
